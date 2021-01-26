4a587c7cfd131d05613fddf139a31c97139e6abc
PC <fake_skype@abv.bg>
2021-01-26 14:05:05 +0200

    yolo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const commands = require('./commands')
 const meme_cmds = require('./meme_cmds')
-//const test = require('./ds_test.js')
+const fight = require('./fight.js')
 
 
 
@@ -11,10 +11,19 @@ const client = new Discord.Client();
 
 
 
+
 client.login(process.env.TOKEN);
+
 client.on('ready', () => {
+
 console.log("Bot ready")
+//status: "online",  // You can show online, idle... Do not disturb is dnd
+client.user.setStatus('busy')
+
+client.user.setActivity('?help', { type: 'LISTENING' });
+
 });
+
 
 
     
