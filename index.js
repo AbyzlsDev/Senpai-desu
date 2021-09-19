f7b0419040dfeb98bd4ce287650286c3d53d6324
PC <fake_skype@abv.bg>
2021-09-20 01:20:00 +0300

    privating bot

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const commands = require('./commands')
+/*const commands = require('./commands')
 const meme_cmds = require('./meme_cmds')
 const fight = require('./fight.js')
 const fight_beta = require('./fight_beta.js')
 const bday = require('./cr_bd.js')
-const tictactoe = require('./tictactoe.js')
 
+const tictactoe = require('./tictactoe.js')*/
 
+require('./reminder.js');
 
 const client = new Discord.Client();
 
@@ -23,15 +24,16 @@ console.log("Bot ready")
 
 
 //status: "online",  // You can show online, idle... Do not disturb is dnd
-client.user.setStatus('busy')
+//client.user.setStatus('Currently for private use - all functions disabled')
+client.user.setPresence({ activity: { name: `Currently for private use - all public functions disabled`, type: "LISTENING" }, status: 'dnd' });
 
-client.user.setActivity('?help', { type: 'LISTENING' });
+//client.user.setActivity('Currently for private use - all functions disabled');
 
 });
 
 
 
-    
+
 
 
 
