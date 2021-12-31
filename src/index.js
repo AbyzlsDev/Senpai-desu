87d47cb07f357fa9ec99b6e8257d7c10151caa4e
PC <fake_skype@abv.bg>
2021-12-31 14:46:57 +0200

    resolve system

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const User = require("./schemas/UserSchema")
 
 
 
+
+
 client.login(process.env.TOKEN);
 
 client.on('ready', () => {
@@ -43,40 +45,130 @@ client.on('message', async (message) => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g)
     const command = args.shift().toLowerCase();
 
-    if(command == "reportuser"){
-
-        var number = Math.random() 
-        number.toString(36); 
-        var id = number.toString(36).substr(2, 36);
-        id.length >= 32; 
-
-         var reportedUser = message.mentions.users.first()
-
-        var reason = args.slice(1).join(' ')
-
-        const newUser = await User.create({
-
-            reportID: id,
-            caseReason: reason,
-            reportedUser: reportedUser,
-            author: message.author.id
 
 
-        })
+  switch(command){
 
-        const savedUser = await newUser.save()
-      
-        console.log(id);
+        case "reportuser":
 
-        const exampleEmbed = new MessageEmbed()
-    .setColor('#0099ff')
-    .setDescription(`Thank you, ${message.author} for reporting ${reportedUser} for reason: ${reason}! The moderators will take action soon!`)
-    .setTimestamp()
+            var reportAuthor = message.author
+
+            var number = Math.random() 
+            number.toString(36); 
+            var id = number.toString(36).substr(2, 36);
+            id.length >= 32; 
     
+             var reportedUser = message.mentions.users.first()
+    
+            var reason = args.slice(1).join(' ')
 
-        message.channel.send(exampleEmbed)
+            if(args[0] == null || args[1] == null) {
+
+                const exampleEmbed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setDescription(`${message.author}, something is missing or misspelled!\n\nCommand syntax: ${`+reportuser <user> <reason>`}`)
+                .setTimestamp()
+            
+                    message.channel.send(exampleEmbed)
+
+                    return
+
+            }
+
+            
+
+            
+    
+            const newUser = await User.create({
+    
+                reportID: id,
+                caseReason: reason,
+                reportedUser: reportedUser,
+                author: reportAuthor
+    
+    
+            })
+    
+            const savedUser = await newUser.save()
+          
+            console.log(id);
+    
+            const exampleEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setDescription(`Thank you, ${message.author} for reporting ${reportedUser} for reason: ${reason}! The moderators will take action soon!`)
+        .setTimestamp()
+
+        const exampleEmbedd = new MessageEmbed()
+        .setColor('#0099ff')
+        .setDescription(`Report ID: ${id}\nReport by: ${reportAuthor.id}\nReported user: ${reportedUser.id}\nReason for report: ${reason}\n`)
+        .setTimestamp()
+
+            client.channels.cache.get('925868590639497271').send(exampleEmbedd);
+            
+    
+            message.channel.send(exampleEmbed)
+
+            
+
+            break
+            
+            case "resolve":
+
+            if(message.member.hasPermission("MANAGE_MESSAGES")){
+
+                var id = args[0]
+
+                User.findOneAndDelete({reportID: id}, (err, data) => {
+    
+                    if(err) throw err
+
+                    else if(data == null) {
+
+                        const exampleEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setDescription(`${message.author}, this ID doesn't exist! Check the ID again! `)
+        .setTimestamp()
+        
+                message.channel.send(exampleEmbed)
+
+                    }
+
+                    else {
+                    
+                       
+
+                     const exampleEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setDescription(`${message.author} resolved ${id}!`)
+        .setTimestamp()
+        
+                client.channels.cache.get('925868590639497271').send(exampleEmbed);
+           
+                    
+                    }
+                })
+              
+
+            }
+            else {
+                const exampleEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setDescription(`${message.author}, you don't have the permissions to resolve this case!`)
+        .setTimestamp()
+        
+                message.channel.send(exampleEmbed)
+            }
+           
+
+            
+
+
+            break
+
 
     }
+
+    
 
 })
 
